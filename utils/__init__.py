80cab6c46fee493709580c16949fa0991bae45ad
Shubhamkumar Patel <shubham3012patel@gmail.com>
2025-04-24 15:12:27 -0400

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
-# Makes the utils directory a Python package
-from .file_parser import process_uploaded_file, generate_download_link
-from .llm_api import get_llm_response, SUPPORTED_MODELS, get_model_capabilities
+# utils/__init__.py
+
+# This file MUST BE EMPTY or contain only comments.
+# Delete any 'from .llm_api import ...' lines.
